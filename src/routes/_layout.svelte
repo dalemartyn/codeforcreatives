3152f4001e7199ec41ea93b043536bf68aa14ccb
Dale <dalemartyn@gmail.com>
2019-07-30 12:08:12 +0100

    copy tutorial from svelte site

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,42 @@
+<script>
+	import { stores } from '@sapper/app';
+	import Icon from '../components/Icon.svelte';
+	import Icons from '../components/Icons.svelte';
+	import Nav from '../components/Nav.svelte';
+	import NavItem from '../components/NavItem.svelte';
+	import PreloadingIndicator from '../components/PreloadingIndicator.svelte';
+
+	const { page, preloading } = stores();
+
+	export let segment;
+</script>
+
+<Icons/>
+
+{#if $preloading}
+	<PreloadingIndicator/>
+{/if}
+
+{#if $page.path !== '/repl/embed'}
+	<Nav {segment} {page}>
+		<NavItem segment="tutorial">Tutorial</NavItem>
+
+		<NavItem external="https://github.com/dalemartyn" title="GitHub Repo">
+			<Icon name="github"/>
+		</NavItem>
+	</Nav>
+{/if}
+
+<main>
+	<slot></slot>
+</main>
+
+<style>
+	main {
+		position: relative;
+		margin: 0 auto;
+		/* padding: var(--nav-h) var(--side-nav) 0 var(--side-nav); */
+		padding: var(--nav-h) 0 0 0;
+		overflow-x: hidden;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Hero from '../components/Hero.svelte';
+</script>
+
+<style>
+</style>
+
+<svelte:head>
+	<title>Code for Creatives</title>
+
+	<meta name="twitter:title" content="Svelte">
+	<meta name="twitter:description" content="Cybernetically enhanced web apps">
+	<meta name="Description" content="Cybernetically enhanced web apps">
+</svelte:head>
+
+<Hero
+	title="Code for Creatives"
+	tagline="Learn to code"
+	outline="svelte-logo-outline.svg"
+	logotype="svelte-logotype.svg"
+/>
