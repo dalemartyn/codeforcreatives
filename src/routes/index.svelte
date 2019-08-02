0a45f867b0368e95fbb676b03f3ea1b32d5a498b
Dale <dalemartyn@gmail.com>
2019-08-02 14:46:52 +0100

    remove home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,31 +1,7 @@
 <script context="module">
-	export async function preload() {
-		alert('preload');
+	export function preload() {
 		this.redirect(302, 'js/commands');
 	}
 </script>
-
-
-<script>
-	import Hero from '../components/Hero.svelte';
-</script>
-
-<style>
-</style>
-
-<svelte:head>
-	<title>Code for Creatives</title>
-
-	<meta name="twitter:title" content="Svelte">
-	<meta name="twitter:description" content="Cybernetically enhanced web apps">
-	<meta name="Description" content="Cybernetically enhanced web apps">
-</svelte:head>
-
-<Hero
-	title="Code for Creatives"
-	tagline="Learn to code"
-	outline="svelte-logo-outline.svg"
-	logotype="svelte-logotype.svg"
-/>
 
 <a href="/js/commands">Start</a>
